928e19ab66bedc73a7021f0b50bb3f6ea6fe692a
Janhavi2001 <janhavisoni2001@gmail.com>
2023-03-14 01:08:14 +0530

    using param-name for one servlet

--- 03userwebapp/src/main/java/com/userweb/servlets/ListUserServlet.java
+++ 03userwebapp/src/main/java/com/userweb/servlets/ListUserServlet.java
@@ -8,21 +8,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-@WebServlet("/listUser")
+
 public class ListUserServlet extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	  private Connection connection;
-	  public void init() {
+	  public void init(ServletConfig config) {
 		  try {
 			  
 			  Class.forName("com.mysql.jdbc.Driver");
-			  connection=DriverManager.getConnection("jdbc:mysql://localhost/mydb","root","root");
+			  String dburl = config.getInitParameter("dburl");
+			  
+			String dbuser = config.getInitParameter("dbuser");
+			
+			String dbpassword = config.getInitParameter("dbpassword");
+			connection=DriverManager.getConnection(dburl,dbuser,dbpassword);
 			  
 		  }
 		  catch(SQLException e) {
